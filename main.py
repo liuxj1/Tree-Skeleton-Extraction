cc69eaa9ff31b3f00d38114f3e7305283a77453d
liuxj1 <1745180299@qq.com>
2024-11-22 20:39:01 +0800

    Update

--- main.py
+++ main.py
@@ -41,6 +41,8 @@ if __name__ == '__main__':
     parser.add_argument('--input_pcd_folder', required=False, help="Path to input folder for batch processing")
     args = parser.parse_args()
 
+    current_script_dir = os.path.dirname(os.path.abspath(__file__))
+
     Processing_mode = args.processing_mode
     random.seed(42)
     np.random.seed(42)
@@ -54,11 +56,11 @@ if __name__ == '__main__':
         if not input_pcd_folder:
             raise ValueError("`input_pcd_folder` must be specified for batch processing.")
 
-        output_MB_pcd_folder = f"datas/{type_name}/MB_pcd"  # Store virtual point cloud files after simulating branch breakage
-        output_skeleton_folder = f"datas/{type_name}/skeleton"  # The complete skeleton folder for the simulated point cloud
-        output_MSBs_folder = f"datas/{type_name}/MSBs"  # Skeleton folder where missing skeleton branches are stored
-        output_BPs_folder = f"datas/{type_name}/BPs"  # Folder for storing branch point pairs used for reconnection
-        output_CMSBs_folder = f"datas/{type_name}/CMSBs"  # Store the completed skeleton
+        output_MB_pcd_folder = os.path.abspath(os.path.join(current_script_dir, f"datas/{type_name}/MB_pcd"))  # Store virtual point cloud files after simulating branch breakage
+        output_skeleton_folder = os.path.abspath(os.path.join(current_script_dir, f"datas/{type_name}/skeleton"))  # The complete skeleton folder for the simulated point cloud
+        output_MSBs_folder = os.path.abspath(os.path.join(current_script_dir, f"datas/{type_name}/MSBs"))  # Skeleton folder where missing skeleton branches are stored
+        output_BPs_folder = os.path.abspath(os.path.join(current_script_dir, f"datas/{type_name}/BPs"))  # Folder for storing branch point pairs used for reconnection
+        output_CMSBs_folder = os.path.abspath(os.path.join(current_script_dir, f"datas/{type_name}/CMSBs"))  # Store the completed skeleton
 
         # 根据 type_name 设置 disconnections_num 和 datasets_type
         if type_name == 'complex':
@@ -84,11 +86,11 @@ if __name__ == '__main__':
             raise ValueError(
                 "`input_pcd_path`, `datasets_type`, and `disconnections_num` must be specified for single processing.")
 
-        output_MB_pcd_folder = "datas/single_data/MB_pcd"  # Store virtual point cloud files after simulating branch breakage
-        output_skeleton_folder = "datas/single_data/skeleton"  # The complete skeleton folder for the simulated point cloud
-        output_MSBs_folder = "datas/single_data/MSBs"  # Skeleton folder where missing skeleton branches are stored
-        output_BPs_folder = "datas/single_data/BPs"  # Folder for storing branch point pairs used for reconnection
-        output_CMSBs_folder = " datas/single_data/CMSBs"  # Store the completed skeleton
+        output_MB_pcd_folder = os.path.abspath(os.path.join(current_script_dir, "datas/single_data/MB_pcd"))  # Store virtual point cloud files after simulating branch breakage
+        output_skeleton_folder = os.path.abspath(os.path.join(current_script_dir, "datas/single_data/skeleton"))  # The complete skeleton folder for the simulated point cloud
+        output_MSBs_folder = os.path.abspath(os.path.join(current_script_dir, "datas/single_data/MSBs"))  # Skeleton folder where missing skeleton branches are stored
+        output_BPs_folder = os.path.abspath(os.path.join(current_script_dir, "datas/single_data/BPs"))  # Folder for storing branch point pairs used for reconnection
+        output_CMSBs_folder = os.path.abspath(os.path.join(current_script_dir, "datas/single_data/CMSBs"))  # Store the completed skeleton
 
         main(input_pcd_path, output_MB_pcd_folder, output_skeleton_folder, output_MSBs_folder,
              output_BPs_folder, output_CMSBs_folder, disconnections_num, datasets_type)
